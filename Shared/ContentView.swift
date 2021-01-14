85f9098b9ea0352ac69d455e3313e739177d4fca
Alexander von Below <alexander.von_below@telekom.de>
2021-01-14 13:16:27 +0100

    Created a standard Xcode Multiplatform project with tests

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexander v. Below on 14.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TestSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSampleApp.swift
+//  Shared
+//
+//  Created by Alexander v. Below on 14.01.21.
+//
+
+import SwiftUI
+
+@main
+struct TestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
